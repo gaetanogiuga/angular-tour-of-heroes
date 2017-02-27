af4efc35689d91fb99241a4bf1da6c86f6584388
Gaetano Giuga <gaetano.giuga@accenture.com>
2017-02-27 17:18:19 +0100

    add hero-detail-component and hero-service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms'
-
+import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 
 @NgModule({
@@ -13,7 +13,8 @@ import { AppComponent }  from './app.component';
 	],
   declarations: 
 	[ 
-		AppComponent 
+		AppComponent,
+		HeroDetailComponent		
 	],
   bootstrap:    
 	[ 
