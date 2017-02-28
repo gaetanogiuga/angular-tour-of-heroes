5937bc51921c20010021eed8233f21da6b6d6276
Gaetano Giuga <gaetano.giuga@accenture.com>
2017-02-28 11:32:07 +0100

    add provider hero.service in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { RouterModule }   from '@angular/router';
-import { DashboardComponent } from './dashboard.component'
+import { NgModule }      		from '@angular/core';
+import { BrowserModule } 		from '@angular/platform-browser';
+import { FormsModule }   		from '@angular/forms';
+import { AppComponent }  		from './app.component';
+import { RouterModule }   		from '@angular/router';
+
+import { HeroDetailComponent } 	from './hero-detail.component';
+import { HeroesComponent }     	from './heroes.component';
+import { DashboardComponent } 	from './dashboard.component'
+import { HeroService }         	from './hero.service';
 
 
 @NgModule({
@@ -36,6 +38,9 @@ import { DashboardComponent } from './dashboard.component'
 		HeroesComponent,
 		DashboardComponent		
 	],
+  providers: [
+    HeroService
+    ],
   bootstrap:    
 	[ 
 		AppComponent 
