952f9d60dec13c4d8df9edb59438cfc06a8ea095
Gaetano Giuga <gaetano.giuga@accenture.com>
2017-02-28 10:25:00 +0100

    heroes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { RouterModule }   from '@angular/router';
+import { DashboardComponent } from './dashboard.component'
 
 
 @NgModule({
   imports:      
 	[ 
 		BrowserModule ,
-		FormsModule
+		FormsModule,
+		RouterModule.forRoot([
+		   {
+			path: '',
+			redirectTo: '/dashboard',
+			pathMatch: 'full'
+		  }, 
+		  {
+			path: 'dashboard',
+			component: DashboardComponent
+		  }, 
+		  {
+			path: 'heroes',
+			component: HeroesComponent
+		  }
+		])
 	],
   declarations: 
 	[ 
 		AppComponent,
-		HeroDetailComponent		
+		HeroDetailComponent, 
+		HeroesComponent,
+		DashboardComponent		
 	],
   bootstrap:    
 	[ 
 		AppComponent 
-	]
+	],
+
 })
+
 export class AppModule { }
