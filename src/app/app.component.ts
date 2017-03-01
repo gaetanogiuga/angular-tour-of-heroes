4c4442048d747a14199218793b97d4957ca22598
Gaetano Giuga <gaetano.giuga@accenture.com>
2017-03-01 15:35:29 +0100

    add style and html in the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,16 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector : 'my-app',
+	moduleId : module.id,
+	styleUrls: ['./app.component.css'],
 	template : `<h1>{{title}}</h1>
 				<nav>
 					<a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
 					<a routerLink="/heroes" routerLinkActive="active">Heroes</a>
 			    </nav>
-				  <router-outlet></router-outlet>
-			 `
+				<router-outlet></router-outlet>
+			 `,
+	
 })
 
 export class AppComponent{
